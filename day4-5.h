6de29e95a714630cdb87fa10369397007744f25b
pavanpm1999 <70958220+pavanpm1999@users.noreply.github.com>
2021-10-09 22:06:04 +0530

    Add files via upload

---
+++ day4-5.h
@@ -0,0 +1,9 @@
+/*int ascending_decsending( int n);
+int swap(int ,int);
+int find_max_min(int size , int *max ,int *min);
+int remove_duplicate(int );
+int linear_search(int size, int key);
+int binary_search(int n ,int key);*/
+int consecutive(int size);
+ int string_length(int size);
+
