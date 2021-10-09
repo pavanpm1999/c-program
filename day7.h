314476eea3a4a44e489bfc8536ab5e207b84ec6e
pavanpm1999 <70958220+pavanpm1999@users.noreply.github.com>
2021-10-09 22:09:36 +0530

    Add files via upload

---
+++ day7.h
@@ -0,0 +1,7 @@
+int malloc_allocation(int n);
+int findLargest(int n);
+int findsmallest(int n);
+int sum_of_array(int n);
+int sum_of_array_even_and_odd(int n);
+int palindrome (int n);
+int memory_deallocation(int n);
