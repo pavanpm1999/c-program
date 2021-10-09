c05b28d197c68a087dc524b0270f6a3abcc2580e
pavanpm1999 <70958220+pavanpm1999@users.noreply.github.com>
2021-10-09 22:02:57 +0530

    Add files via upload

---
+++ day2.h
@@ -0,0 +1,18 @@
+// Enter a 5 digit number and find the sum of its digits. For E.g. if int number = 12345
+int  sum_of_digit(int );
+int reverse_digit(int );
+int occurrence_of_number(int , int );
+int palindrome_or_not(int);
+int prime_number(int );
+int prime_number_up_to_limit(int );
+int find_sum_series(int digit);
+int checkForArmstrong(int num);
+int  areAmicable(int x, int y);
+int sum_difference_product(int a, int b);
+float cube(int a);
+float cuboid(int l, int b, int h);
+float cylinder(int r, int h);
+float sphere( int r);
+float cone(int r, int h);
+int volume_of_shape( int len , int b, int h ,int a,int r);
+int unit_price(int unit);
