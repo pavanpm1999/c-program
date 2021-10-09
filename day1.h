af62aaea475a2a436eed38cb35989e30f185c27c
pavanpm1999 <70958220+pavanpm1999@users.noreply.github.com>
2021-10-09 22:01:49 +0530

    Add files via upload

---
+++ day1.h
@@ -0,0 +1,14 @@
+//Write a program that converts upper case to lower.
+ int convert_upper_to_lower( char );
+ // Write a program to calculate the area of circle.
+ int area_of_circle( int );
+// Write a program to calculate simple and compound interest
+int simple_interest(int ,float , float);
+int compound_interest(int, float, int);
+//Write a program to convert temperature from Celsius to Fahrenheit and Fahrenheit to Celsius
+int Celsius_to_Fahrenheit(int );
+int Fahrenheit_to_Celsius (int);
+//Check if entered number is even or odd.
+int even_or_odd(int);
+//Check if entered year is leap year or not.
+int number_is_leap_or_not(int);
