f08dbe476191f9298c6d8fe46fb6dcb4b52ae5b5
pavanpm1999 <70958220+pavanpm1999@users.noreply.github.com>
2021-10-09 22:15:59 +0530

    Add files via upload

---
+++ main8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"day8.h"
+#include"assert.h"
+int main()
+{
+    assert(student_information(2)==0);
+    assert(point_x_and_y(3)==0);
+    return 0;
+}
