fe1da62dc8477e3e314563abf9a7d29c0426e0be
pavanpm1999 <70958220+pavanpm1999@users.noreply.github.com>
2021-10-09 22:11:46 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-#include<assert.h>
-#include "day4-5.h"
 #include <stdlib.h>
-
+#include"day8.h"
+#include"assert.h"
 int main()
-{ /* int size=0;
-    int array[size];
-assert(ascending_decsending(6)==0);
-assert(swap(12,13)==0);
-assert(find_max_min( 6 , &array[5] ,&array[0])==0);
-assert(remove_duplicate(10)==0);
-assert(linear_search(6, 1)==6);
-assert(binary_search(5,4)== 0);*/
-assert (consecutive(5)==0);
-assert(string_length(5)==0);
+{
+    assert(student_information(2)==0);
+    assert(point_x_and_y(3)==0);
     return 0;
 }
